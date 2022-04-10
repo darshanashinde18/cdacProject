5799379a76a45ea070647cfd313684461d0e25ce
darshanashinde18 <99385217+darshanashinde18@users.noreply.github.com>
2022-04-10 14:56:39 +0530

    Delete DepartmentRepository.java

--- project_backend3/src/main/java/com/app/dao/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-
-}
